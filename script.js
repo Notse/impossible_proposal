c5c225209baa7ac1d9318e40c52fb63b35ae1fc5
Nikhil Notse <nikhilnotse@yahoo.com>
2026-02-13 02:58:29 +0530

    initial commit

--- script.js
+++ script.js
@@ -7,60 +7,64 @@ const Audio = document.querySelector("#audio");
 const switchAudio = document.querySelector("#audio-switch");
 const Wrapper = document.querySelector(".wrapper");
 const lyricsContainer = document.getElementById("lyrics-1");
+const proposal_text = document.getElementById("proposal-text");
+
 
 let count = 0;
 let sec = 2000;
 
 Checkbox.addEventListener("click", Checked);
-document.addEventListener("DOMContentLoaded", () => {
-  swal(
-    "Try to stop the audio 😁",
-    "I challenge you to stop the audio by switching off checkbox , Let see if you can do it.",
-    {
-      buttons: {
-        Play: "Let's Play",
-      },
-    }
-  ).then((value) => {
-    switch (value) {
-      default:
-        Audio.play();
-        lyricsContainer.style.display = "block";
-    }
-  });
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   swal(
+//     "Hey You can't say No To Me 😁",
+//     "Hello girl , The most handsome man in the world wants to be your valentine.",
+//     {
+//       buttons: {
+//         Play: "Click here to proceed",
+//       },
+//     }
+//   ).then((value) => {
+//     switch (value) {
+//       default:
+//         // Audio.play();
+//         // lyricsContainer.style.display = "block";
+//     }
+//   });
+// });
 
 function Checked() {
   switchAudio.play();
   if (Checkbox.checked === true) {
-    Audio.play();
+    // Audio.play();
     setTimeout(function () {
       console.log("1");
       hand.style.display = "none";
       hanumanArm.style.opacity = "1";
       hanumanArm.classList.remove("arm-move");
+      
     }, 1000);
   } else {
     if (count >= 0) {
-      Hanuman.style.transform = "translate(0, -30px) rotate(-5deg) scaleX(-1)";
+      Hanuman.style.transform = "translate(-30px, -5vh) rotate(-5deg) scaleX(1)";
       sec = 2100;
       setTimeout(function () {
         Hanuman.style.transform =
-          "translate(0, 300px) rotate(-5deg) scaleX(-1)";
+          "translate(0, 30vh) rotate(-5deg) scaleX(1)";
         Wrapper.style.display = "none";
       }, 4400);
       hanumanArm.style.transition = "transform 1s ease-in";
+       proposal_text.textContent = 'Will you be my valentine?'
     }
     Wrapper.style.display = "block";
     count = count + 1;
     hanumanArm.classList.add("arm-move");
     hanumanArm.style.transition = "transform 1.5s ease-in";
-    Audio.pause();
+    // Audio.pause();
     setTimeout(function () {
       console.log("2");
       hanumanArm.style.opacity = "0";
       hand.style.display = "block";
-
+      proposal_text.textContent = '“Mujhe pta tha tum Yes hi bologe 😎”'
       Checkbox.click();
     }, sec);
   }
